8bad3d16d2ed33fdf83388669d1611cff7c62bf6
dmmonir2003 <dmmonir2003@gmail.com>
2024-01-10 22:33:04 +0600

    Services Dynamic Part 1

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('ello');
+
+
+const LoadServices=()=>{
+    fetch('https://testing-8az5.onrender.com/services/')
+    .then(res=>res.json())
+    .then(data=>DisplayServies(data))
+    .catch(error=>console.log(error))
+}
+
+const DisplayServies=(services)=>{
+        services.forEach(service => {
+            console.log(service);
+        });
+}
+
+
+LoadServices();
